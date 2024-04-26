12fc3382ecb877c077b5753f9989b4fddef03b89
Marcin Mennemann <marcinmennemann@gmail.com>
2024-04-26 23:03:30 +0200

    amend

--- src/bentley_ottmann.cpp
+++ src/bentley_ottmann.cpp
@@ -3,8 +3,22 @@
 #include "Vec2.h"
 
 #include <vector>
+#include <queue>
+#include <set>
 
 using namespace std;
+
+template <typename T>
+struct event_point {
+    Vec2 p;
+    segment<T>* parent;
+    segment<T>* intersect;
+
+    bool operator>(const event_point& other) const {
+        if (p.x == other.p.x) return p.y > other.p.y;
+        return p.x > other.p.x;
+    }
+};
 
 
 // solves ax + by = c
@@ -48,14 +62,42 @@ Vec2* find_intersect(segment<T>* a, segment<T>* b) {
     return res;
 }
 
+// assuming:
+//          No two line segment endpoints or crossings have the same x-coordinate
+//          No line segment endpoint lies upon another line segment
+//          No three line segments intersect at a single point.
+template <typename F>
+vector<intersection<F>> bentley_ottmann(vector<segment<F>> segments) {
+    priority_queue<event_point<F>, vector<event_point<F>>, greater<event_point<F>>> Q;
+    set<segment<F>*> T;
 
-template <typename T>
-vector<intersection<T>> bentley_ottmann(vector<segment<T>>) {
-    
+    for (segment<F>& s : segments) {
+        Q.push({s.lp, &s, NULL});
+        Q.push({s.rp, &s, NULL});
+    }
+
+    vector<intersection<F>> intersections;
+
+    while(!Q.empty()) {
+        event_point<F> p = Q.top();
+        Q.pop();
+
+        cout << "now visiting " << p.p << endl;
+    }
+
+    return intersections;
 }
 
 
 
-void test() {
+void test() { 
+    segment<char> a{{1,4},{4,1}, 'a'};
+    segment<char> b{{3,1},{6,5}, 'b'};
+    segment<char> c{{7,4},{12,2}, 'c'};
+    segment<char> d{{5,3},{17,4}, 'd'};
+    segment<char> e{{9,5},{11,1}, 'e'};
 
+
+
+    bentley_ottmann<char>({d,b,a,c,e});
 }
